d148b5f05d566fa01f73cee23c7cefb75a7a96ef
Taimoor Changaiz <taimoor.changaiz@gmail.com>
2013-10-07 22:47:50 +0500

    inital files commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Multichilds::Application.config.secret_token = '45466125f136d42184a22ccc05cddf3f816b6f65acd385b532f4ca404bdf1fde4d1c186cc0769322dc64193cdf2df2a41db2bff80c94e247feed742c4b1ace16'
